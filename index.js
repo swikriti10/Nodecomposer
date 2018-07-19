18fe0928d6120f238c005fd6f64e8f26b278fb0a
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-07-19 09:25:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
-
 "use strict";
 
 const express = require("express");
 const bodyParser = require("body-parser");
 var http = require('http');
 var request = require('request');
-var request1 = require('request');
-var request2 = require('request');
-var c3;
-var botResponse;
-var botResponse1;
-var result;
-var entity;
-var a;
-var a1;
+var session = require('express-session');
+var csrfToken;
+
+
 var c;
-var c1;
-var entity1;
-var obj = [];
-var entity2 = {};
+
+
 const restService = express();
 
 const App = require('actions-on-google').DialogflowApp;
-
-var app = express();
-//var url = "http://208.85.249.174:8000/sap/opu/odata/CRVWM/WMS_SRV";
-var url1 = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
-//ar url1 = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
-var url = "http://208.85.249.174:8000/sap/opu/odata/CRVWM/WMS_SRV";
+var slack_message;
 
 
-var d = 4500001002;
+var url = "http://208.85.249.174:8000/sap/opu/odata/CRVWM/WMS_SRV/";
+var url1 = "https://wiprowms30june-b94b9a0ad.dispatcher.us1.hana.ondemand.com/WMS900/sap/opu/odata/CRVWM/WMS_SRV/";
+
+//var d = '1140';
 var i = 0;
 var obj = [];
+var botResponse = "";
 restService.use(
   bodyParser.urlencoded({
       extended: true
@@ -40,17 +31,17 @@ restService.use(
 );
 
 restService.use(bodyParser.json());
+//restService.use(session({ secret: 'ssshhhhh' }));
+//var sess;
 
 
-app.get('/test', function (req, res) {
-
-
+restService.post("/wms", function (req, res) {
     var array =
      req.body.result &&
       req.body.result.parameters &&
       req.body.result.parameters.array
         ? req.body.result.parameters.array
-       : "noarray";
+       : "notselectedmenu";
 
     return res.json({
         speech: array,
@@ -58,23 +49,29 @@ app.get('/test', function (req, res) {
         // speech: optionIntentname,
         // displayText: optionIntentname,
         source: "webhook-echo-sample"
-        
+
 
     });
-    
 
-
-  
 
    
 
+    
 
 
 
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.listen(process.env.PORT || 8000, function () {
+
+
+
+
+
+
+
+
+restService.listen(process.env.PORT || 8000, function () {
     console.log("Server up and listening");
 });
-
