e3e70b1fb1e750e52c64cf7568465a0b317e854f
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-07-30 15:59:04 -0500

    Update network.js

--- network/network.js
+++ network/network.js
@@ -14,7 +14,7 @@ let adminConnection;
 //this is the business network connection the tests will use.
 let businessNetworkConnection;
 
-let businessNetworkName = 'clp-network';
+let businessNetworkName = 'admin@product-auction';
 let factory;
 
 
@@ -64,341 +64,35 @@ async function useIdentity(cardName) {
 //export module
 module.exports = {
 
-  /*
-  * Create Member participant and import card for identity
-  * @param {String} cardId Import card id for member
-  * @param {String} accountNumber Member account number as identifier on network
-  * @param {String} firstName Member first name
-  * @param {String} lastName Member last name
-  * @param {String} phoneNumber Member phone number
-  * @param {String} email Member email
-  */
- registerMember: async function (cardId, accountNumber,firstName, lastName, email, phoneNumber) {
-    try {
-
-      //connect as admin
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect('admin@clp-network');
-
-      //get the factory for the business network
-      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-
-      //create member participant
-      const member = factory.newResource(namespace, 'Member', accountNumber);
-      member.firstName = firstName;
-      member.lastName = lastName;
-      member.email = email;
-      member.phoneNumber = phoneNumber;
-      member.points = 0;
-
-      //add member participant
-      const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Member');
-      await participantRegistry.add(member);
-
-      //issue identity
-      const identity = await businessNetworkConnection.issueIdentity(namespace + '.Member#' + accountNumber, cardId);
-
-      //import card for identity
-      await importCardForIdentity(cardId, identity);
-
-      //disconnect
-      await businessNetworkConnection.disconnect('admin@clp-network');
-
-      return true;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error;
-    }
-
-  },
-
-  /*
-  * Create Partner participant and import card for identity
-  * @param {String} cardId Import card id for partner
-  * @param {String} partnerId Partner Id as identifier on network
-  * @param {String} name Partner name
-  */
-  registerPartner: async function (cardId, partnerId, name) {
+/*
+* Get Member data
+* @param {String} cardId Card id to connect to network
+* @param {String} accountNumber Account number of member
+*/
+memberData: async function (cardId, accountNumber) {
 
     try {
 
-      //connect as admin
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect('admin@clp-network');
+        //connect to network with cardId
+        businessNetworkConnection = new BusinessNetworkConnection();
+        await businessNetworkConnection.connect(cardId);
 
-      //get the factory for the business network.
-      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+        //get member from the network
+        const memberRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Member');
+        const member = await memberRegistry.get(accountNumber);
 
-      //create partner participant
-      const partner = factory.newResource(namespace, 'Partner', partnerId);
-      partner.name = name;
+        //disconnect
+        await businessNetworkConnection.disconnect(cardId);
 
-      //add partner participant
-      const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Partner');
-      await participantRegistry.add(partner);
-
-      //issue identity
-      const identity = await businessNetworkConnection.issueIdentity(namespace + '.Partner#' + partnerId, cardId);
-
-      //import card for identity
-      await importCardForIdentity(cardId, identity);
-
-      //disconnect
-      await businessNetworkConnection.disconnect('admin@clp-network');
-
-      return true;
+        //return member object
+        return member;
     }
     catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error;
+        //print and return error
+        console.log(err);
+        var error = {};
+        error.error = err.message;
+        return error;
     }
-
-  },
-
-  /*
-  * Perform EarnPoints transaction
-  * @param {String} cardId Card id to connect to network
-  * @param {String} accountNumber Account number of member
-  * @param {String} partnerId Partner Id of partner
-  * @param {Integer} points Points value
-  */
-  earnPointsTransaction: async function (cardId, accountNumber, partnerId, points) {
-
-    try {
-
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //get the factory for the business network.
-      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-
-      //create transaction
-      const earnPoints = factory.newTransaction(namespace, 'EarnPoints');
-      earnPoints.points = points;
-      earnPoints.member = factory.newRelationship(namespace, 'Member', accountNumber);
-      earnPoints.partner = factory.newRelationship(namespace, 'Partner', partnerId);
-
-      //submit transaction
-      await businessNetworkConnection.submitTransaction(earnPoints);
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      return true;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error;
-    }
-
-  },
-
-  /*
-  * Perform UsePoints transaction
-  * @param {String} cardId Card id to connect to network
-  * @param {String} accountNumber Account number of member
-  * @param {String} partnerId Partner Id of partner
-  * @param {Integer} points Points value
-  */
-  usePointsTransaction: async function (cardId, accountNumber, partnerId, points) {
-
-    try {
-
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //get the factory for the business network.
-      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-
-      //create transaction
-      const usePoints = factory.newTransaction(namespace, 'UsePoints');
-      usePoints.points = points;
-      usePoints.member = factory.newRelationship(namespace, 'Member', accountNumber);
-      usePoints.partner = factory.newRelationship(namespace, 'Partner', partnerId);
-
-      //submit transaction
-      await businessNetworkConnection.submitTransaction(usePoints);
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      return true;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error
-    }
-
-  },
-
-  /*
-  * Get Member data
-  * @param {String} cardId Card id to connect to network
-  * @param {String} accountNumber Account number of member
-  */
-  memberData: async function (cardId, accountNumber) {
-
-    try {
-
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //get member from the network
-      const memberRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Member');
-      const member = await memberRegistry.get(accountNumber);
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      //return member object
-      return member;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error;
-    }
-
-  },
-
-  /*
-  * Get Partner data
-  * @param {String} cardId Card id to connect to network
-  * @param {String} partnerId Partner Id of partner
-  */
-  partnerData: async function (cardId, partnerId) {
-
-    try {
-
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //get member from the network
-      const partnerRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Partner');
-      const partner = await partnerRegistry.get(partnerId);
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      //return partner object
-      return partner;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error
-    }
-
-  },
-
-  /*
-  * Get all partners data
-  * @param {String} cardId Card id to connect to network
-  */
-  allPartnersInfo : async function (cardId) {
-
-    try {
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //query all partners from the network
-      const allPartners = await businessNetworkConnection.query('selectPartners');
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      //return allPartners object
-      return allPartners;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error
-    }
-  },
-
-  /*
-  * Get all EarnPoints transactions data
-  * @param {String} cardId Card id to connect to network
-  */
-  earnPointsTransactionsInfo: async function (cardId) {
-
-    try {
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //query EarnPoints transactions on the network
-      const earnPointsResults = await businessNetworkConnection.query('selectEarnPoints');
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      //return earnPointsResults object
-      return earnPointsResults;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error
-    }
-
-  },
-
-  /*
-  * Get all UsePoints transactions data
-  * @param {String} cardId Card id to connect to network
-  */
-  usePointsTransactionsInfo: async function (cardId) {
-
-    try {
-      //connect to network with cardId
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect(cardId);
-
-      //query UsePoints transactions on the network
-      const usePointsResults = await businessNetworkConnection.query('selectUsePoints');
-
-      //disconnect
-      await businessNetworkConnection.disconnect(cardId);
-
-      //return usePointsResults object
-      return usePointsResults;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error
-    }
-
-  }
 
 }
