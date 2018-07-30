8829bc40341509c6f26dee3ceb5cb877d51020bd
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-07-30 15:46:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const router = express.Router();
 
 //get the libraries to call
 var network = require('./network/network.js');
-var validate = require('./network/validate.js');
-var analysis = require('./network/analysis.js');
+
 
 //bootstrap application settings
 app.use(express.static('./public'));
@@ -51,178 +50,7 @@ app.get('/about', function(req, res) {
 });
 
 
-//post call to register member on the network
-app.post('/api/registerMember', function(req, res) {
 
-  //declare variables to retrieve from request
-  var accountNumber = req.body.accountnumber;
-  var cardId = req.body.cardid;
-  var firstName = req.body.firstname;
-  var lastName = req.body.lastname;
-  var email = req.body.email;
-  var phoneNumber = req.body.phonenumber;
-
-  //print variables
-  console.log('Using param - firstname: ' + firstName + ' lastname: ' + lastName + ' email: ' + email + ' phonenumber: ' + phoneNumber + ' accountNumber: ' + accountNumber + ' cardId: ' + cardId);
-
-  //validate member registration fields
-  validate.validateMemberRegistration(cardId, accountNumber, firstName, lastName, email, phoneNumber)
-    .then((response) => {
-      //return error if error in response
-      if (response.error != null) {
-        res.json({
-          error: response.error
-        });
-        return;
-      } else {
-        //else register member on the network
-        network.registerMember(cardId, accountNumber, firstName, lastName, email, phoneNumber)
-          .then((response) => {
-            //return error if error in response
-            if (response.error != null) {
-              res.json({
-                error: response.error
-              });
-            } else {
-              //else return success
-              res.json({
-                success: response
-              });
-            }
-          });
-      }
-    });
-
-
-});
-
-//post call to register partner on the network
-app.post('/api/registerPartner', function(req, res) {
-
-  //declare variables to retrieve from request
-  var name = req.body.name;
-  var partnerId = req.body.partnerid;
-  var cardId = req.body.cardid;
-
-  //print variables
-  console.log('Using param - name: ' + name + ' partnerId: ' + partnerId + ' cardId: ' + cardId);
-
-  //validate partner registration fields
-  validate.validatePartnerRegistration(cardId, partnerId, name)
-    .then((response) => {
-      //return error if error in response
-      if (response.error != null) {
-        res.json({
-          error: response.error
-        });
-        return;
-      } else {
-        //else register partner on the network
-        network.registerPartner(cardId, partnerId, name)
-          .then((response) => {
-            //return error if error in response
-            if (response.error != null) {
-              res.json({
-                error: response.error
-              });
-            } else {
-              //else return success
-              res.json({
-                success: response
-              });
-            }
-          });
-      }
-    });
-
-});
-
-//post call to perform EarnPoints transaction on the network
-app.post('/api/earnPoints', function(req, res) {
-
-  //declare variables to retrieve from request
-  var accountNumber = req.body.accountnumber;
-  var cardId = req.body.cardid;
-  var partnerId = req.body.partnerid;
-  var points = parseFloat(req.body.points);
-
-  //print variables
-  console.log('Using param - points: ' + points + ' partnerId: ' + partnerId + ' accountNumber: ' + accountNumber + ' cardId: ' + cardId);
-
-  //validate points field
-  validate.validatePoints(points)
-    .then((checkPoints) => {
-      //return error if error in response
-      if (checkPoints.error != null) {
-        res.json({
-          error: checkPoints.error
-        });
-        return;
-      } else {
-        points = checkPoints;
-        //else perforn EarnPoints transaction on the network
-        network.earnPointsTransaction(cardId, accountNumber, partnerId, points)
-          .then((response) => {
-            //return error if error in response
-            if (response.error != null) {
-              res.json({
-                error: response.error
-              });
-            } else {
-              //else return success
-              res.json({
-                success: response
-              });
-            }
-          });
-      }
-    });
-
-});
-
-//post call to perform UsePoints transaction on the network
-app.post('/api/usePoints', function(req, res) {
-
-  //declare variables to retrieve from request
-  var accountNumber = req.body.accountnumber;
-  var cardId = req.body.cardid;
-  var partnerId = req.body.partnerid;
-  var points = parseFloat(req.body.points);
-
-  //print variables
-  console.log('Using param - points: ' + points + ' partnerId: ' + partnerId + ' accountNumber: ' + accountNumber + ' cardId: ' + cardId);
-
-  //validate points field
-  validate.validatePoints(points)
-    //return error if error in response
-    .then((checkPoints) => {
-      if (checkPoints.error != null) {
-        res.json({
-          error: checkPoints.error
-        });
-        return;
-      } else {
-        points = checkPoints;
-        //else perforn UsePoints transaction on the network
-        network.usePointsTransaction(cardId, accountNumber, partnerId, points)
-          .then((response) => {
-            //return error if error in response
-            if (response.error != null) {
-              res.json({
-                error: response.error
-              });
-            } else {
-              //else return success
-              res.json({
-                success: response
-              });
-            }
-          });
-      }
-    });
-
-
-});
 
 //post call to retrieve member data, transactions data and partners to perform transactions with from the network
 app.post('/api/memberData', function(req, res) {
@@ -256,128 +84,15 @@ app.post('/api/memberData', function(req, res) {
       }
 
     })
-    .then(() => {
-      //get UsePoints transactions from the network
-      network.usePointsTransactionsInfo(cardId)
-        .then((usePointsResults) => {
-          //return error if error in response
-          if (usePointsResults.error != null) {
-            res.json({
-              error: usePointsResults.error
-            });
-          } else {
-            //else add transaction data to return object
-            returnData.usePointsResults = usePointsResults;
-          }
-
-        }).then(() => {
-          //get EarnPoints transactions from the network
-          network.earnPointsTransactionsInfo(cardId)
-            .then((earnPointsResults) => {
-              //return error if error in response
-              if (earnPointsResults.error != null) {
-                res.json({
-                  error: earnPointsResults.error
-                });
-              } else {
-                //else add transaction data to return object
-                returnData.earnPointsResult = earnPointsResults;
-              }
-
-            })
-            .then(() => {
-              //get partners to transact with from the network
-              network.allPartnersInfo(cardId)
-              .then((partnersInfo) => {
-                //return error if error in response
-                if (partnersInfo.error != null) {
-                  res.json({
-                    error: partnersInfo.error
-                  });
-                } else {
-                  //else add partners data to return object
-                  returnData.partnersData = partnersInfo;
-                }
+    
 
                 //return returnData
                 res.json(returnData);
 
-              });
-            });
-        });
-    });
+              
 
 });
 
-//post call to retrieve partner data and transactions data from the network
-app.post('/api/partnerData', function(req, res) {
-
-  //declare variables to retrieve from request
-  var partnerId = req.body.partnerid;
-  var cardId = req.body.cardid;
-
-  //print variables
-  console.log('partnerData using param - ' + ' partnerId: ' + partnerId + ' cardId: ' + cardId);
-
-  //declare return object
-  var returnData = {};
-
-  //get partner data from network
-  network.partnerData(cardId, partnerId)
-    .then((partner) => {
-      //return error if error in response
-      if (partner.error != null) {
-        res.json({
-          error: partner.error
-        });
-      } else {
-        //else add partner data to return object
-        returnData.id = partner.id;
-        returnData.name = partner.name;
-      }
-
-    })
-    .then(() => {
-      //get UsePoints transactions from the network
-      network.usePointsTransactionsInfo(cardId)
-        .then((usePointsResults) => {
-          //return error if error in response
-          if (usePointsResults.error != null) {
-            res.json({
-              error: usePointsResults.error
-            });
-          } else {
-            //else add transaction data to return object
-            returnData.usePointsResults = usePointsResults;
-            //add total points collected by partner to return object
-            returnData.pointsCollected = analysis.totalPointsCollected(usePointsResults);
-          }
-
-        })
-        .then(() => {
-          //get EarnPoints transactions from the network
-          network.earnPointsTransactionsInfo(cardId)
-            .then((earnPointsResults) => {
-              //return error if error in response
-              if (earnPointsResults.error != null) {
-                res.json({
-                  error: earnPointsResults.error
-                });
-              } else {
-                //else add transaction data to return object
-                returnData.earnPointsResults = earnPointsResults;
-                //add total points given by partner to return object
-                returnData.pointsGiven = analysis.totalPointsGiven(earnPointsResults);
-              }
-
-              //return returnData
-              res.json(returnData);
-
-            });
-        });
-    });
-
-});
 
 //declare port
 var port = process.env.PORT || 8000;
