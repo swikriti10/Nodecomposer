01c011b5df00f70f18c351f03cb84720e51a9457
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-08-03 13:28:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,107 +1,193 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
-var http = require('http');
-var request = require('request');
-var session = require('express-session');
-var csrfToken;
+
+
+//get libraries
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const request = require('request');
+
+const path = require('path');
+
+
+
+//create express web-app
+
+const app = express();
+
+const router = express.Router();
+
 const AdminConnection = require('composer-admin').AdminConnection;
+
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 
 const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
 
-//declate namespace
-const namespace = 'org.acme.product.auction';
-
 //in-memory card store for testing so cards are not persisted to the file system
+
 const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
 
+
+
 //admin connection to the blockchain, used to deploy the business network
+
 let adminConnection;
 
-//this is the business network connection the tests will use.
-let businessNetworkConnection;
-
-let businessNetworkName = 'product-auction';
-let factory;
-
-var c;
+var namespace='org.acme.product.auction'
 
 
-const restService = express();
-
-const App = require('actions-on-google').DialogflowApp;
-
-//var d = '1140';
-var i = 0;
-var obj = [];
-var botResponse = "";
-restService.use(
-  bodyParser.urlencoded({
-      extended: true
-  })
-);
-
-restService.use(bodyParser.json());
-//restService.use(session({ secret: 'ssshhhhh' }));
-//var sess;
 
 
-restService.get("/check", function (req, res) {
-   var accountNumber ='testx@gmail.com';
-    var cardId ='PeerAdmin@hlfv1';
 
-    //print variables
-    console.log('memberData using param - ' + ' accountNumber: ' + accountNumber + ' cardId: ' + cardId);
 
-    //declare return object
-    var returnData = {};
-  
-  
-  try {
 
-        //connect to network with cardId
-        businessNetworkConnection = new BusinessNetworkConnection();
-        businessNetworkConnection.connect('PeerAdmin@hlfv1','admin@product-auction','admin','adminpw');
-    
-    const memberRegistry =  businessNetworkConnection.getParticipantRegistry(namespace + '.Member');
-        const member =  memberRegistry.get(accountNumber);
-    returnData.email=member.email;
-    returnData.firstName=member.firstName;
+//get the libraries to call
 
-        //disconnect
-        // businessNetworkConnection.disconnect(cardId);
-//
-        
-    
-res.send("returnData");
-        //return member object
-        
-    }
-    catch(err) {
-        //print and return error
-        console.log(err);
-        var error = {};
-        error.error = err.message;
-        res.send(error);
-    }
+var network = require('./network/network.js');
 
-  
-  
-  
-  
-  
-  
-   // var items =[{name:'swik',location:'Texas'},
-           //     {name:'Tinku',location:'Texas'}
-  //  ];
-   //  res.send(items);
-  
-  
+
+
+
+
+//bootstrap application settings
+
+
+
+
+
+
+
+
+
+//post call to retrieve member data, transactions data and partners to perform transactions with from the network
+
+app.get('/api/memberData', function(req, res) {
+
+
+
+//declare variables to retrieve from request
+
+var accountNumber ='testx@gmail.com';
+
+//var cardId ='admin@product-auction';
+
+var cardId ='testx@product-auction';
+
+
+//await importCardForIdentity(cardId, identity);
+
+
+
+
+
+
+
+//let businessNetworkConnection;
+
+// businessNetworkConnection = new BusinessNetworkConnection();
+
+// businessNetworkConnection.connect(cardId);
+
+//print variables
+
+console.log('memberData using param - ' + ' accountNumber: ' + accountNumber + ' cardId: ' + cardId);
+
+
+
+//declare return object
+
+var returnData = {};
+
+network.memberData(cardId, accountNumber)
+
+.then((member) => {
+
+//return error if error in response
+
+if (member.error != null) {
+
+console.log(member.error);
+
+
+
+res.json({
+
+error: member.error
 
 });
 
-restService.listen(process.env.PORT || 8000, function () {
-    console.log("Server up and listening");
+} else {
+
+//else add member data to return object
+
+returnData.email = member.email;
+
+returnData.firstName = member.firstName;
+
+returnData.lastName = member.lastName;
+
+}
+
+console.log('a');
+
+console.log(returnData.email);
+
+console.log('a');
+
+
+
+})
+
+
+
+
+
+//get member data from network
+
+///////const memberRegistry = businessNetworkConnection.getParticipantRegistry(namespace + '.Member');
+
+//const member = memberRegistry.get(accountNumber);
+
+//returnData.email=member.email;
+
+//returnData.firstName=member.firstName;
+
+
+
+//return returnData
+
+res.json(returnData);
+
+
+
+
+
+
+});
+
+
+
+
+
+//declare port
+
+var port = process.env.PORT || 8000;
+
+if (process.env.VCAP_APPLICATION) {
+
+port = process.env.PORT;
+
+}
+
+
+
+//run app on port
+
+app.listen(port, function() {
+
+console.log('app running on port: %d', port);
+
 });
